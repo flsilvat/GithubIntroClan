114fb92ad8c3fc7f90af37012f3cbb387fb63ac4
Francisco Silva <fsilva@aeroman.com.sv>
2021-03-12 15:54:44 -0600

    Initializing

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CollaborativeProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            //FSilva - Escribir cualquier linea de texto
+            Console.WriteLine("Primera Linea!\n");
+
+            //OAguirre - Escribir cualquier linea de texto
+
+
+        }
+    }
+}
